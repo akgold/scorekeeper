c7eabe0bb43b811d3ffc6a28e4b8e5e975e00a20
Alex Gold <agold@analystinstitute.org>
2016-12-27 13:20:36 -0500

    Add updated init

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,7 @@ import datetime
 import os
 
 app = Flask(__name__)
-CONFIG_LOC = '/Users/agold/Documents/scorekeeper/app/config.py'
-app.config.from_envvar('CONFIG_LOC')
+app.config.from_object('app.config')
 db = SQLAlchemy(app)
 
 from app import models, views
