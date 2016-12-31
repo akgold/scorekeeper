764bf4ac4ced4c2a44ead219e0a7bb666549b26b
Xaq Rothman <xaq@radcampaign.com>
2016-12-31 11:30:57 -0500

    JS, style, and template updates for showing actual data.

--- app/static/src/index.js
+++ app/static/src/index.js
@@ -1,15 +1,25 @@
 import * as d3 from 'd3';
 import _ from 'underscore';
 
-d3.json('static/dummy.json', draw);
+function roundPoints(d) {
+  return Math.round(d.points * 1000)/1000;
+}
+
+function update() {
+  d3.json('get_scoreboard', draw);
+}
+
+update();
 
 function draw(err, data) {
   if (err) {
-    console.log(error);
+    update();
   }
   else {
-    const fontScale = d3.scaleLinear().range([1, 3]).domain(d3.extent(_.pluck(data, 'score')));
-    const fontMap = d => fontScale(d.score) + 'em';
+    data = _.sortBy(data, d => -d.points);
+
+    const fontScale = d3.scaleLinear().range([1, 3]).domain(d3.extent(_.pluck(data, 'points')));
+    const fontMap = d => fontScale(d.points) + 'em';
     const rows = d3.select('tbody').selectAll('tr').data(data, d => d.name);
 
     rows.enter()
@@ -18,26 +28,21 @@ function draw(err, data) {
       .each(function (d) {
         const row = d3.select(this);
         row.append('td').text(d => d.name);
-        row.append('td').classed('score', true).text(d.score);
+        row.append('td').classed('points', true).text(roundPoints);
       })
       ;
 
     rows
       .style('font-size', fontMap)
       .each(function (d) {
-        d3.select(this).select('td.score').text(d.score);
+        d3.select(this).select('td.points').text(roundPoints);
       })
       ;
 
-    rows.sort((a, b) => (b.score - a.score));
+    rows.sort((a, b) => (b.points - a.points));
 
-    window.setTimeout(draw, 2000, null, randomize(data));
+    rows.exit().remove();
+
+    window.setTimeout(update, 5000);
   }
-}
-
-function randomize(data) {
-  return _.map(data, d => {
-    d.score += Math.round(Math.random() * 10);
-    return d;
-  });
 }
